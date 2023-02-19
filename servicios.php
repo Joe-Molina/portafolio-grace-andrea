466830e15f5d09a833ee6ee330c509ff63353e55
Joe Molina <joedodaniljr123@gmail.com>
2023-02-19 15:53:09 -0400

    actualizacion de diseño en los card container

--- servicios.php
+++ servicios.php
@@ -5,37 +5,49 @@
     <div class="card-container">
 
         <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
-        </div>
-        <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
-        </div>
-        <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
-        </div>
-        <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
+            <div class="imagen-container">
+            <img src="imagenes/1.jpg" alt="imagen proyecto">
+            </div>
+            <div class="card-text"><h2>emotes</h2></div>
+            <div class="table tabla-emotes">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>cantidad</th>
+                            <th>precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>6</td>
+                            <td>5$ </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
-         <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
-        </div>
         <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
-        </div>
-        <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
-        </div>
-        <div class="service-card">
-            <img src="imagenes/1.jpg" alt="">
-            <div class="card-text"><h2>servicio</h2></div>
+            <div class="imagen-container">
+            <img src="imagenes/1.jpg" alt="imagen proyecto">
+            </div>
+            <div class="card-text"><h2>emotes</h2></div>
+            <div class="table tabla-emotes">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>cantidad</th>
+                            <th>precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>6</td>
+                            <td>5$ </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
     </div>
